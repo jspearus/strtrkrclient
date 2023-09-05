1ef9eb60c45d11a364699f3987031261395acb65
jspearus <35698087+jspearus@users.noreply.github.com>
2023-09-05 13:55:55 -0500

    update

--- tempJson.py
+++ tempJson.py
@@ -2,7 +2,7 @@ import json
 import os
 import datetime as dt
 
-def json_store(pal, case, alsle, store, slot):
+def record_order(pal, case, alsle, store, slot):
     file_time = dt.datetime.now()
     date = file_time.strftime("%m:%d:%Y:%H:%M")
     with open(f'order_{date}.json', 'w', encoding='utf-8') as f:
@@ -12,4 +12,4 @@ def json_store(pal, case, alsle, store, slot):
         json.dump(data, f, ensure_ascii=False, indent=4)
         
 #  test
-json_store(12, 350, 41, 52, 10)
+record_order(12, 350, 41, 52, 10)
---
+++ time_api.py
@@ -0,0 +1,19 @@
+import requests
+import json, os
+from pathlib import Path
+
+api_url = "http://ordr.jspearlab.com/api/timestores/"
+
+
+def get_store_times():
+    # record = {"NumPalets": 4.0,  "NumItems": 250, "categories":'41-2, 421-1.1', "StoreNum": 20, "Lane": 10 }
+    header = {"Content-Type":"application/json"}
+    response = requests.get(api_url, headers=header)
+
+    print (response.status_code)
+    print(response.json())
+    with open("store_times.json", "w") as outfile:
+        outfile.write(json.dumps(response.json(), indent=4))
+    
+#test function
+get_store_times()
